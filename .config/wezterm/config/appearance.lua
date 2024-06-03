9ce9cf49a11ad1eafa7643f97391f94aabfd857f
CrazyCatZhang <crazycatzhang@outlook.com>
2024-06-03 23:51:38 +0800

    🔧 chore: update bg opacity

--- .config/wezterm/config/appearance.lua
+++ .config/wezterm/config/appearance.lua
@@ -37,7 +37,7 @@ return {
          source = { Color = colors.background },
          height = '100%',
          width = '100%',
-         opacity = 0.96,
+         opacity = 0.98,
       },
    },
 
