059f18568329ca2ebb0c3bd2af5edf9e4ebc042e
CrazyCatZhang <crazycatzhang@outlook.com>
2024-06-07 21:14:45 +0800

    🐛 fix: symbol usage display error

--- .config/lvim/lua/user/theme.lua
+++ .config/lvim/lua/user/theme.lua
@@ -603,11 +603,11 @@ M.telescope_theme = function(colorset)
       return vim.api.nvim_get_hl(0, { name = name })
     end
 
-    vim.api.nvim_set_hl(0, "SymbolUsageRounding", { fg = h("TabLine").bg, italic = true })
-    vim.api.nvim_set_hl(0, "SymbolUsageContent", { bg = h("TabLine").bg, fg = h("TabLine").fg, italic = true })
-    vim.api.nvim_set_hl(0, "SymbolUsageRef", { fg = h("Function").fg, bg = h("TabLine").bg, italic = true })
-    vim.api.nvim_set_hl(0, "SymbolUsageDef", { fg = h("Type").fg, bg = h("TabLine").bg, italic = true })
-    vim.api.nvim_set_hl(0, "SymbolUsageImpl", { fg = h("@keyword").fg, bg = h("TabLine").bg, italic = true })
+    vim.api.nvim_set_hl(0, "SymbolUsageRounding", { fg = h("CursorLine").bg, italic = true })
+    vim.api.nvim_set_hl(0, "SymbolUsageContent", { bg = h("CursorLine").bg, fg = h("Comment").fg, italic = true })
+    vim.api.nvim_set_hl(0, "SymbolUsageRef", { fg = h("Function").fg, bg = h("CursorLine").bg, italic = true })
+    vim.api.nvim_set_hl(0, "SymbolUsageDef", { fg = h("Type").fg, bg = h("CursorLine").bg, italic = true })
+    vim.api.nvim_set_hl(0, "SymbolUsageImpl", { fg = h("@keyword").fg, bg = h("CursorLine").bg, italic = true })
   end
 end
 
