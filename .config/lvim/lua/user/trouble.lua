aa08d5dd506b92fe9abd7f895188f49c2ecb60c5
CrazyCatZhang <crazycatzhang@outlook.com>
2024-06-16 22:00:33 +0800

    🔧 chore: set auto_jump in trouble to fasle

--- .config/lvim/lua/user/trouble.lua
+++ .config/lvim/lua/user/trouble.lua
@@ -18,6 +18,19 @@ M.config = function()
       size = { width = 0.4, height = 0.3 },
       zindex = 200,
     },
+    modes = {
+      lsp_references = { auto_jump = false },
+      lsp_definitions = { auto_jump = false },
+      lsp_implementations = { auto_jump = false },
+      lsp_declarations = { auto_jump = false },
+      lsp_incoming_calls = { auto_jump = false },
+      lsp_outgoing_calls = { auto_jump = false },
+
+      diagnostics_buffer = {
+        mode = "diagnostics", -- inherit from diagnostics mode
+        filter = { buf = 0 }, -- filter diagnostics to the current buffer
+      },
+    },
   }
 
   if lvim.transparent_window then
