e7c60c181fa5f166f573c9879a88b19624e6564a
CrazyCatZhang <crazycatzhang@outlook.com>
2024-06-29 22:41:58 +0800

    ⚗️ chore: change git config directory

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -63,3 +63,4 @@ export PATH="$PATH:/Users/crazycatzhang/Library/Application Support/Coursier/bin
 
 export PATH=$(echo "$PATH" | awk -v RS=':' -v ORS=":" '!seen[$0]++' | sed 's/:$//')
 
+export GIT_CONFIG_GLOBAL="$HOME/.config/git/.gitconfig"
